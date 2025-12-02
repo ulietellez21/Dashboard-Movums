3d8f93a137c1395726f329429e0a8c7d39146f71
Ulises Téllez <ulisestellez@MacBook-Air-de-Ulises.local>
2025-12-01 23:30:13 -0600

    reviviendo server

--- agencia_web/settings.py
+++ agencia_web/settings.py
@@ -15,12 +15,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '[REDACTED_SECRET_KEY]'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', '[REDACTED_SECRET_KEY]')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
 
 
 # Application definition
